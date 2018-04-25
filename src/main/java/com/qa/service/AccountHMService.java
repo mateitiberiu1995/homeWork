0052412b8b2b459d1e79bedeb825286e3094ec5a
CapMaster <CapMaster@Alina-PC.mynet>
2018-04-25 23:00:27 +0100

    initial pass

--- src/main/java/com/qa/service/AccountHMService.java
+++ src/main/java/com/qa/service/AccountHMService.java
@@ -1,9 +1,21 @@
 package com.qa.service;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.qa.domain.Account;
+import com.qa.util.JSONUtil;
+
 public class AccountHMService {
 
+	private Map<Long, Account> accountMap  = new HashMap<Long,Account>();
+	private JSONUtil util = new JSONUtil();
+	
 	public String addAccount(String account)
 	{
-		return "";
+		Account anAccount = util.getObjectForJSON(account , Account.class);
+		accountMap.put((long) (accountMap.size()+1), anAccount);
+		return "{\"message\": \"account sucessfully added\"}";
+
 	}
 }
