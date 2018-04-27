684fcaf480eb01a8c3b4707a49620a0197279517
Tiberiu Matei <mateitiberiu1995@gmail.com>
2018-04-27 14:36:42 +0100

    JAVAX

---
+++ src/main/java/com/qa/integration/JAVActivator.java
@@ -0,0 +1,9 @@
+package com.qa.integration;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class JAVActivator extends Application{
+
+}
