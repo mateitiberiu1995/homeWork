b19b1fb5b4f3acc15e49954cb1d820092b36cbbc
Tiberiu Matei <mateitiberiu1995@gmail.com>
2018-04-27 14:38:55 +0100

    getAllAccounts function

---
+++ src/main/java/com/qa/integration/AccountServiceEndPoint.java
@@ -0,0 +1,27 @@
+package integration;
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+
+import com.qa.service.AccountService;
+
+
+@Path("/account")
+public class AccountServiceEndPoint {
+
+	@Inject
+	private AccountService repo;
+	
+	
+	@GET
+	@Path("/json")
+	@Produces({ "application/json" })
+	public String getAllAccounts()
+	{
+		return repo.getAllAccounts();
+	}
+}
